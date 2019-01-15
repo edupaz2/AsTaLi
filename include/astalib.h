7b42c7ffe727a2ccd40a8b0957a50492c5debe32
Eduardo Paz <edupaz2@gmail.com>
2019-01-15 23:42:11 +0800

    fix: Splitting headers into two files for easier sharing internally

--- include/astalib.h
+++ include/astalib.h
@@ -1,19 +1,7 @@
 #ifndef __ASTALIB_H__
 #define __ASTALIB_H__
 
-#include <cstdint>
-#include <vector>
-
-using task_id = std::int32_t; // [MININT,-1] Error codes. [0, MAXINT] Task ids
-using task_type = std::uint32_t;
-struct STaskStatus
-{
-	task_id 	id;
-	task_type 	type;
-	enum status { paused, running, stopped, completed };
-
-};
-using task_status = std::vector<STaskStatus>;
+#include "astalib_defs.h"
 
 task_id 		startTask	();
 task_id 		startTask	(const task_type& type);
---
+++ include/astalib_defs.h
@@ -0,0 +1,18 @@
+#ifndef _ASTALIB_DEFS_H_
+#define _ASTALIB_DEFS_H_
+
+#include <cstdint>
+#include <vector>
+
+using task_id = std::uint32_t; // [0,49] Error codes. [50, MAXINT] Task ids
+using task_type = std::uint32_t;
+struct STaskStatus
+{
+	task_id 	id;
+	task_type 	type;
+	enum status { paused, running, stopped, completed };
+
+};
+using task_status = std::vector<STaskStatus>;
+
+#endif
